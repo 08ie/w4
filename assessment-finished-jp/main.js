5b43abe466735992a46136f9db7bbc4ebb509137
08ie <113569652+08ie@users.noreply.github.com>
2022-09-29 15:21:44 +0900

    Update main.js

--- assessment-finished-jp/main.js
+++ assessment-finished-jp/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '外は華氏94度でした。だから :insertx: 散歩しに行った。 :inserty:に行ったとき、彼らは怖がり始めた。そして、:insertz:。ボブは全てを見たが、全然驚かなかった。というのも、:insertx:の体重は３００ポンドで、暑い日だった。';
+const storyText = '外は華氏94度でした。だから :insertx:は散歩しに行った。 :inserty:に行ったとき、彼らは怖がり始めた。そして、:insertz:。ボブは全てを見たが、全然驚かなかった。というのも、:insertx:の体重は３００ポンドで、暑い日だった。';
 const insertX = ['ゴブリン・ウィリー', 'ビッグダディ', 'クリスマス父さん'];
 const insertY = ['炊き出し', 'Disneyland', 'ホワイトハウス'];
 const insertZ = ['自然発火', '歩道の水たまりに溶けた', 'ナメクジになって這い去った'];
