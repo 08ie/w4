460577105add315b136a237631e1319daa918da3
08ie <113569652+08ie@users.noreply.github.com>
2022-09-29 15:20:58 +0900

    Update main.js

--- assessment-finished-jp/main.js
+++ assessment-finished-jp/main.js
@@ -8,9 +8,9 @@ function randomValueFromArray(array){
 }
 
 const storyText = '外は華氏94度でした。だから :insertx: 散歩しに行った。 :inserty:に行ったとき、彼らは怖がり始めた。そして、:insertz:。ボブは全てを見たが、全然驚かなかった。というのも、:insertx:の体重は３００ポンドで、暑い日だった。';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const insertX = ['ゴブリン・ウィリー', 'ビッグダディ', 'クリスマス父さん'];
+const insertY = ['炊き出し', 'Disneyland', 'ホワイトハウス'];
+const insertZ = ['自然発火', '歩道の水たまりに溶けた', 'ナメクジになって這い去った'];
 
 randomize.addEventListener('click', result);
 
